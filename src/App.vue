953e3cad31378a32ce04c72a4f1e4049609c89cd
Hugo Alliaume <kocal@live.fr>
2018-08-21 14:37:26 +0200

    configure store

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
+    {{ foo }}
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapGetters(['foo'])
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    foo: 'bar'
+  },
+  getters: {
+    foo: state => state.foo
   },
   mutations: {
 
